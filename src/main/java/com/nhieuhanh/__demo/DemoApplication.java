9df0f68b080ea267c2870971c80e1831f1d7e196
MinhBDD <24521081@gm.uit.edu.vn>
2026-04-22 14:45:59 +0700

    cập nhật

--- src/main/java/com/nhieuhanh/__demo/DemoApplication.java
+++ src/main/java/com/nhieuhanh/__demo/DemoApplication.java
@@ -1,12 +1,12 @@
 package com.nhieuhanh.__demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;//class này giúp khởi động ứng dụng Spring Boot
+import org.springframework.boot.autoconfigure.SpringBootApplication;//đánh dấu đây là class chính để khởi động ứng dụng Spring Boot
 
-@SpringBootApplication
+@SpringBootApplication//Đây là điểm bắt đầu của demo, hệ thống sẽ chạy từ đây
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
+    }// Lệnh chạy ứng dụng Spring Boot, quét tất cả các lớp liên quan là book và book controller và truy cập vào link này http://localhost:8081/swagger-ui.html
 }
